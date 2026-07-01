601705f6b14cc2ecc02243eb558d857e13698aa3
ishika30k <ishikakh30@gmail.com>
2026-07-01 23:38:03 +0530

    made changes to nav section

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg h-16 bg-primary/40 content-center'>
             <div className='mx-auto justify-center c-space max-w-7xl'>
                 <div className='flex items-center justify-between py-2 sm:py-0'>
-                    <a href="/" className='text-3xl font-bold transition-colors text-neutral-400 hover:text-white'>ish</a>
+                    <a href="/" className='text-2xl font-bold transition-colors text-neutral-400 hover:text-white'>✦ISHIKA</a>
                     <button
                         onClick={()=> setIsOpen(!isOpen)}
                         className='flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden'
